2d36d239ea93a96fbddcbecf54b96f0d879dfa67
ThanhHai2510 <toilathanhhaitep@gmail.com>
2020-03-16 15:00:23 +0700

    5 files modified, 2 added

---
+++ src/app/MockData.ts
@@ -0,0 +1,18 @@
+import { product } from './Product';
+export const data: product =[
+  {
+    name: 'Xiaomi',
+    price: 200,
+    desc: 'China'
+  },
+  {
+    name: 'Samsung',
+    price: 300,
+    desc: 'Korean'
+  },
+  {
+    name: 'Apple',
+    price: 400,
+    desc: 'America'
+  }
+]
